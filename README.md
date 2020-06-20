407ad7578c3aec4a342fcd50abf0fa0d5ec33f54
reuschj <reuschj@gmail.com>
2020-06-20 14:01:03 -0500

    First commit

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # AppBackgroundView
 
-A description of this package.
+This package is an easy way to wrap content within a full-screen (edge-to-edge) background.
+
+```swift
+AppBackgroundView(Color.blue) {
+    // Your content goes here
+    VStack {
+        Text("Hello, World!")
+    }
+}
+```
--- Sources/AppBackgroundView/AppBackgroundView.swift
+++ Sources/AppBackgroundView/AppBackgroundView.swift
@@ -1,3 +1,52 @@
-struct AppBackgroundView {
-    var text = "Hello, World!"
+//
+//  AppBackgroundView.swift
+//  AnalogClock
+//
+//  Created by Justin Reusch on 5/29/20.
+//  Copyright © 2020 Justin Reusch. All rights reserved.
+//
+
+import SwiftUI
+
+/// Wraps content within a full-screen (edge-to-edge) background
+@available(iOS 13.0, *)
+@available(OSX 10.15, *)
+public struct AppBackgroundView<Content, S>: View where Content : View, S: ShapeStyle {
+    
+    /// The fill for the background
+    public var fill: S = Color.clear as! S
+    
+    /// The fill style
+    public var style: FillStyle = FillStyle()
+    
+    /// Stores the content function builder
+    public var content: () -> Content
+    
+    // Initializer ---------------------------- /
+    
+    /**
+     - Parameter fill: The fill for the background
+     - Parameter style: The fill style
+     - Parameter content: The view builder content to pass
+     */
+    @inlinable public init(
+        _ fill: S = Color.clear as! S,
+        style: FillStyle = FillStyle(),
+        @ViewBuilder content: @escaping () -> Content
+    ) {
+        self.fill = fill
+        self.style = style
+        self.content = content
+    }
+    
+    // Body ---------------------------- /
+    
+    public var body: some View {
+        ZStack {
+            Rectangle()
+                .fill(fill)
+                .edgesIgnoringSafeArea(.all)
+            content()
+        }
+    }
 }
